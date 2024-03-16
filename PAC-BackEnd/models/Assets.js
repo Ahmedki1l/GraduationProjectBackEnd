4476f7f283c933dbfdf9527f62e56b6e2e2dccd1
أحمد علاء الدين عادل أحمد <136151580+SinSintro@users.noreply.github.com>
2024-03-16 05:53:23 +0200

    Fetching to the front end

--- PAC-BackEnd/models/Assets.js
+++ PAC-BackEnd/models/Assets.js
@@ -3,8 +3,10 @@ const Assets = [
     //developer 1
     {
         assetId: 1,
+        image: "/public/temp image.jpeg",
         title: "asset 1",
         size: "150m",
+        price: 50000,
         noOfBath: 2,
         noOfBed: 3,
         locationId: 1,
@@ -12,8 +14,10 @@ const Assets = [
     },
     {
         assetId: 2,
+        image: "/public/temp image.jpeg",
         title: "asset 2",
         size: "100m",
+        price: 50000,
         noOfBath: 1,
         noOfBed: 3,
         locationId: 1,
@@ -21,8 +25,10 @@ const Assets = [
     },
     {
         assetId: 3,
+        image: "/public/temp image.jpeg",
         title: "asset 3",
         size: "120m",
+        price: 50000,
         noOfBath: 2,
         noOfBed: 3,
         locationId: 1,
@@ -30,8 +36,10 @@ const Assets = [
     },
     {
         assetId: 4,
+        image: "/public/temp image.jpeg",
         title: "asset 4",
         size: "90m",
+        price: 50000,
         noOfBath: 1,
         noOfBed: 2,
         locationId: 1,
@@ -40,8 +48,10 @@ const Assets = [
     //developer 2
     {
         assetId: 5,
+        image: "/public/temp image.jpeg",
         title: "asset 5",
         size: "150m",
+        price: 50000,
         noOfBath: 2,
         noOfBed: 3,
         locationId: 2,
@@ -49,8 +59,10 @@ const Assets = [
     },
     {
         assetId: 6,
+        image: "/public/temp image.jpeg",
         title: "asset 6",
         size: "100m",
+        price: 50000,
         noOfBath: 1,
         noOfBed: 3,
         locationId: 2,
@@ -58,8 +70,10 @@ const Assets = [
     },
     {
         assetId: 7,
+        image: "/public/temp image.jpeg",
         title: "asset 7",
         size: "150m",
+        price: 50000,
         noOfBath: 2,
         noOfBed: 4,
         locationId: 2,
@@ -67,8 +81,10 @@ const Assets = [
     },
     {
         assetId: 8,
+        image: "/public/temp image.jpeg",
         title: "asset 8",
         size: "90m",
+        price: 50000,
         noOfBath: 1,
         noOfBed: 2,
         locationId: 2,
@@ -77,8 +93,10 @@ const Assets = [
     //developer 3
     {
         assetId: 9,
+        image: "/public/temp image.jpeg",
         title: "asset9",
         size: "70m",
+        price: 50000,
         noOfBath: 1,
         noOfBed: 2,
         locationId: 3,
@@ -86,8 +104,10 @@ const Assets = [
     },
     {
         assetId: 10,
+        image: "/public/temp image.jpeg",
         title: "asset 10",
         size: "150m",
+        price: 50000,
         noOfBath: 2,
         noOfBed: 3,
         locationId: 3,
@@ -95,8 +115,10 @@ const Assets = [
     },
     {
         assetId: 11,
+        image: "/public/temp image.jpeg",
         title: "asset 11",
         size: "100m",
+        price: 50000,
         noOfBath: 1,
         noOfBed: 3,
         locationId: 3,
@@ -104,8 +126,10 @@ const Assets = [
     },
     {
         assetId: 12,
+        image: "/public/temp image.jpeg",
         title: "asset 12",
         size: "150m",
+        price: 50000,
         noOfBath: 2,
         noOfBed: 4,
         locationId: 3,
@@ -113,8 +137,10 @@ const Assets = [
     },
     {
         assetId: 13,
+        image: "/public/temp image.jpeg",
         title: "asset 13",
         size: "70m",
+        price: 50000,
         noOfBath: 1,
         noOfBed: 2,
         locationId: 3,
--- PAC-BackEnd/models/Developers.js
+++ PAC-BackEnd/models/Developers.js
@@ -1,18 +1,24 @@
 const Developers = [
     {
         developerId: 1,
+        image: "public/TMG.jpg",
+        coverImage: "public/Sell-Image.jpg",
         name: "developer 1",
         noOfProjects: -1,
         age: 70
     },
     {
         developerId: 2,
+        image: "public/TMG.jpg",
+        coverImage: "public/Sell-Image.jpg",
         name: "developer 2",
         noOfProjects: -1,
         age: 50
     },
     {
         developerId: 3,
+        image: "public/TMG.jpg",
+        coverImage: "public/Sell-Image.jpg",
         name: "developer 3",
         noOfProjects: -1,
         age: 100
